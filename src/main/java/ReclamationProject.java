3bbc4fd6599472c9ceb8a28382a639c1fa219b96
jesse <jesse@LAPTOP-AGOSEK61>
2017-09-26 12:44:07 -0500

    unsure of how to correctly add javadoc comment, but attempt was made

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -7,32 +7,36 @@
  * 4. Add comments and Javadoc comments where needed
  * 5. Remove unnecessary comments as appropriate
  */
-/*
- * returns a if it is contained in string b
- * returns an empty string if a is not contained in b
- * @param a potential substring of a
- * @param b potentially contains a
- * @return a if a is contained in b
- * @see String
+/**
+ * returns a if it is contained in string b.
+ * returns an empty string if a is not contained in b.
  */
 public class ReclamationProject {
-    static String doit(String a,String b) {
-
+    /**
+     * returns a if it is contained in string b.
+     * returns an empty string if a is not contained in b.
+     * @param a is a cool string
+     * @param b potentially contains a
+     * @return a if a is contained in b
+     */
+    static String doit(final String a, final String b) {
+        String first = a;
+        String second = b;
         if (a.length() > b.length()) { // if a is longer than b, switch two variables using temp c
             String c = a;
-            a = b;
-            b = c;
+            first = b;
+            second = c;
         }
         String r = "";
-        for (int i = 0; i < a.length(); i++) {
+        for (int i = 0; i < first.length(); i++) {
             //works forward through string a
-            for (int j = a.length() - i; j > 0; j--) {
+            for (int j = first.length() - i; j > 0; j--) {
                 //works backwards through string a
-                for (int k = 0; k < b.length() - j; k++) {
+                for (int k = 0; k < second.length() - j; k++) {
                     //works backwards through string b
-                    if (a.regionMatches(i, b, k, j) && j > r.length()) {
+                    if (first.regionMatches(i, second, k, j) && j > r.length()) {
                         //changed to a conditional statement
-                        r = a.substring(i, i + j);
+                        r = first.substring(i, i + j);
                     }
                 }
             }
