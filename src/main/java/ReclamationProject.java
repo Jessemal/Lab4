c6f31485575b5b93caaa179b36451b1e12eae434
jesse <jesse@LAPTOP-AGOSEK61>
2017-09-26 12:30:50 -0500

    unsure of how to correctly add a javadoc comment, but an attempt was made

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -7,20 +7,35 @@
  * 4. Add comments and Javadoc comments where needed
  * 5. Remove unnecessary comments as appropriate
  */
+/*
+ * returns a if it is contained in string b
+ * returns an empty string if a is not contained in b
+ * @param a potential substring of a
+ * @param b potentially contains a
+ * @return a if a is contained in b
+ * @see String
+ */
+public class ReclamationProject {
+    static String doit(String a,String b) {
 
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
-        /*
-         * For loop with i
-         */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+        if (a.length() > b.length()) { // if a is longer than b, switch two variables using temp c
+            String c = a;
+            a = b;
+            b = c;
+        }
+        String r = "";
+        for (int i = 0; i < a.length(); i++) {
+            //works forward through string a
+            for (int j = a.length() - i; j > 0; j--) {
+                //works backwards through string a
+                for (int k = 0; k < b.length() - j; k++) {
+                    //works backwards through string b
+                    if (a.regionMatches(i, b, k, j) && j > r.length()) {
+                        //changed to a conditional statement
+                        r = a.substring(i, i + j);
+                    }
+                }
+            }
+        } return r;
+    }
 }
